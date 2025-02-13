193663af97c460b0d08c72ca0d808f06d8a6b831
Dan <dwestlund222@hotmail.com>
2025-02-13 15:35:04 -0600

    Add comments to the code. Add local storage and saving.

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ let context = canvas.getContext('2d');
 
 canvas.width = 640;
 canvas.height = 800;
-canvas.style.border = '5px solid red';
+canvas.style.border = '5px solid green';
 let canvas_width = canvas.width;
 let canvas_height = canvas.height;
-let isAddingShape = false;
-let isMovingNewShape = false;
-let shapeToAdd = null;
+
+// ADD SHAPE variables
+let isAddingShape = false; // are we in the state of adding a shape
+let isMovingNewShape = false; // are we in the state of moving a new shape
+let shapeToAdd = null; // object to store temp shape to be added
 
 // Calculate offset of canvas from browser window
 let offset_x;
@@ -35,11 +37,37 @@ canvas.onresize = function () {
 
 // SHAPES
 let shapes = [];
-let current_shape_index = null;
-let is_draggin = false;
+let current_shape_index = null; // used to find current shape in shapes array
+let is_draggin = false; // is the current shape being dragged
 let startX;
 let startY;
-shapes.push(new Player({ position: { x: 0, y: 0 }, number: 0 }));
+
+// STORAGE
+let data = {
+  shapes: [new Player({ position: { x: 0, y: 0 }, number: 0 })],
+};
+
+let reset_local_storage = function () {
+  localStorage.removeItem('data');
+  shapes = [];
+  draw_shapes();
+};
+let initialize_local_storage = function () {
+  if (localStorage.getItem('data')) {
+    data = JSON.parse(localStorage.getItem('data'));
+    for (let shape of data.shapes) {
+      shapes.push(new Player(shape));
+    }
+  } else {
+    localStorage.setItem('data', JSON.stringify(data));
+  }
+};
+initialize_local_storage();
+
+let saveData = function () {
+  data = { shapes };
+  localStorage.setItem('data', JSON.stringify(data));
+};
 
 // ADD SHAPE
 document.addEventListener('keydown', function (event) {
@@ -47,22 +75,27 @@ document.addEventListener('keydown', function (event) {
     isAddingShape = false;
     isMovingNewShape = false;
     shapeToAdd = null;
+    resetShapes();
     draw_shapes();
   }
 });
 
+// State is set to adding a shape
 let add_shape = function (event) {
   isAddingShape = true;
 };
 
+// State is set to normal dragging mode
 let allow_move = function (event) {
   isAddingShape = false;
   isMovingNewShape = false;
   shapeToAdd = null;
+  resetShapes();
   draw_shapes();
 };
 
 // MOUSE EVENTS
+// Check if mouse is in shape so it can be dragged
 let is_mouse_in_shape = function (x, y, shape) {
   let shape_left = shape.position.x;
   let shape_right = shape.position.x + shape.width;
@@ -75,11 +108,15 @@ let is_mouse_in_shape = function (x, y, shape) {
   return false;
 };
 
+// Handle all mouse down events
 let mouse_down = function (event) {
   if (isAddingShape) {
+    // isAddingShape is only true at the very start of adding a new shape, and goes to false once we are moving that shape
+    // this check is so the code for checking if mouse_down is in a shape does not run
     return;
   }
 
+  // if we are moving a new shape, then we need to add it to the shapes array
   if (isMovingNewShape) {
     shapeToAdd.color = 'rgba(255,0,0,1)';
     shapes.push(shapeToAdd);
@@ -89,9 +126,11 @@ let mouse_down = function (event) {
     return;
   }
 
+  // Calculate mouse position and allow dragging if mouse is over a shape
   startX = parseInt(event.clientX - offset_x);
   startY = parseInt(event.clientY - offset_y);
   let index = 0;
+  resetShapes();
   for (let shape of shapes) {
     if (is_mouse_in_shape(startX, startY, shape)) {
       current_shape_index = index;
@@ -100,30 +139,31 @@ let mouse_down = function (event) {
       draw_shapes();
       return;
     }
-
     index++;
   }
 };
 
+// Handle all mouse up events
 let mouse_up = function (event) {
-  if (!is_draggin) {
-    return;
+  if (is_draggin) {
+    event.preventDefault();
+    is_draggin = false;
+    draw_shapes();
+    saveData();
   }
-  event.preventDefault();
-  is_draggin = false;
-  shapes[current_shape_index].isMoving = false;
-  draw_shapes();
 };
 
+// Handle all mouse out events
 let mouse_out = function (event) {
-  if (!is_draggin) {
-    return;
+  if (is_draggin) {
+    event.preventDefault();
+    is_draggin = false;
   }
-  event.preventDefault();
-  is_draggin = false;
 };
 
+// Handle all mouse move events
 let mouse_move = function (event) {
+  // draging existing shape
   if (is_draggin) {
     event.preventDefault();
     let mouseX = parseInt(event.clientX - offset_x);
@@ -140,7 +180,9 @@ let mouse_move = function (event) {
     draw_shapes();
     startX = mouseX;
     startY = mouseY;
-  } else if (isAddingShape) {
+  }
+  // initial check to add a new shape before changing state to isMovingNewShape = true
+  else if (isAddingShape) {
     event.preventDefault();
     let mouseXx = parseInt(event.clientX - offset_x);
     let mouseYx = parseInt(event.clientY - offset_y);
@@ -154,7 +196,9 @@ let mouse_move = function (event) {
       isMovingNewShape = true;
       isAddingShape = false;
     }
-  } else if (isMovingNewShape) {
+  }
+  // moving new shape that has not bee placed yet
+  else if (isMovingNewShape) {
     let mouseX = parseInt(event.clientX - offset_x);
     let mouseY = parseInt(event.clientY - offset_y);
 
@@ -170,10 +214,18 @@ let mouse_move = function (event) {
   }
 };
 
+// EVENT LISTENERS
 canvas.onmousedown = mouse_down;
 canvas.onmouseup = mouse_up;
 canvas.onmouseout = mouse_out;
 canvas.onmousemove = mouse_move;
+
+// RESET SHAPES to default values
+let resetShapes = function () {
+  for (let shape of shapes) {
+    shape.isMoving = false;
+  }
+};
 
 // MAIN DRAWING FUNCTION
 let draw_shapes = function () {
